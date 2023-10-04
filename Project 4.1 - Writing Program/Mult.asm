432260206f2ef8402ff9142fe27ff50f7a8806fc
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-10-04 23:17:02 +0530

    Add files via upload

---
+++ Project 4.1 - Writing Program/Mult.asm
@@ -0,0 +1,39 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+(BEGIN)
+	// Initialize product to 0.
+	@R2
+	M=0
+
+(LOOP)
+	//If R1 <= 0, terminate.
+	@R1
+	D=M
+	@END
+	D;JLE
+	//Add R0 to R2.
+	@R0
+	D=M
+	@R2
+	M=M+D
+	//Decerement R1.
+	@R1
+	M=M-1
+	// Loop.
+	@LOOP
+	0;JMP
+
+(END)
+	//Terminate.
+	@END
+	0;JMP
